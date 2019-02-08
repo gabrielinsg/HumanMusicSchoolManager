4df0542abad09d16c489b5605f4d423f9b0282a0
Gabriel de Oliveira Santos <Gabriel_insg@hotmail.com>
2019-02-08 18:10:45 -0200

    HMusicSchool_0802181810

--- HumanMusicSchoolManager/Controllers/DispSalaController.cs
+++ HumanMusicSchoolManager/Controllers/DispSalaController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HumanMusicSchoolManager.Models.ViewModels;
+using HumanMusicSchoolManager.Services;
 using HumanMusicSchoolManager.ServicesInterface;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,17 +13,30 @@ namespace HumanMusicSchoolManager.Controllers
     {
         private readonly IDispSalaService _dispSalaService;
         private readonly IPacoteCompraService _pacoteCompraService;
+        private readonly ICursoService _cursoService;
 
         public DispSalaController(IDispSalaService dispSalaService,
-            IPacoteCompraService pacoteCompraService)
+            IPacoteCompraService pacoteCompraService,
+            ICursoService cursoService)
         {
             this._dispSalaService = dispSalaService;
             this._pacoteCompraService = pacoteCompraService;
+            this._cursoService = cursoService;
         }
 
         public IActionResult Index()
         {
             return View(_dispSalaService.BuscarTodos());
+        }
+
+        public IActionResult HorariosDisponiveis()
+        {
+            var horariosDisponiveis = new HorariosDisponiveisViewModel
+            {
+                DispSalas = _dispSalaService.HorariosDisponiveis(),
+                Cursos = _cursoService.BuscarTodos()
+            };
+            return View(horariosDisponiveis);
         }
     }
 }
---
+++ HumanMusicSchoolManager/Models/ViewModels/HorariosDisponiveisViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HumanMusicSchoolManager.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HumanMusicSchoolManager.Models.ViewModels
+{
+    public class HorariosDisponiveisViewModel
+    {
+        public List<DispSala> DispSalas { get; set; }
+        public List<Curso> Cursos { get; set; }
+    }
+}
