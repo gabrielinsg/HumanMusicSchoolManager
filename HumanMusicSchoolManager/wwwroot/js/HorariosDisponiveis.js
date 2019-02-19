e6c442884fd01011868da94ff8c264e01927eb96
Gabriel de Oliveira Santos <Gabriel_insg@hotmail.com>
2019-02-19 16:21:07 -0300

    HMusicSchool_1902191621

--- HumanMusicSchoolManager/wwwroot/js/HorariosDisponiveis.js
+++ HumanMusicSchoolManager/wwwroot/js/HorariosDisponiveis.js
@@ -1,7 +1,5 @@
-﻿$(document).ready(function () {
-
+﻿
     $("#porSala").on('change', function () {
         let salaId = $("#porSala").val();
         window.location.href = '/DispSala/HorariosDisponiveis?salaId=' + salaId;
     });
-});
