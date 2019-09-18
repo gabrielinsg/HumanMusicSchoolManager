8197a1ccbffe1e55c572f2d427bfeb5d0591cbc8
Gabriel de Oliveira Santos <Gabriel_insg@hotmail.com>
2019-09-18 15:13:43 -0300

    New Layout. Login and Recovery password

---
+++ HumanMusicSchoolManager/Models/Models/ScriptTags.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HumanMusicSchoolManager.Models.Models
+{
+    public class ScriptTags
+    {
+        public string GoogleMapsApiKey { get; set; }
+        public string GoogleTagManager { get; set; }
+        public string GoogleAnalytics { get; set; }
+        public string Facebook { get; set; }
+        public string FacebookOpenGraph { get; set; }
+    }
+}
